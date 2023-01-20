2d51aa602c58d1a96b79c16b2d3b8a5cfb2abfec
JackMolter <molterjack@gamil.com>
2023-01-19 21:42:00 -0500

    added send_packet function

--- UART.c
+++ UART.c
@@ -4,7 +4,8 @@
 
 #include "UART.h"
 
-uint8_t data[12] = {3,32,2,4,12,34,3,7,8,9,19,0};
+// some example data
+uint8_t data[12] = {0,1,2,3,4,5,6,7,8,9,10,11};
 
 
 void uart_setup() {
@@ -19,6 +20,7 @@ void uart_setup() {
     // This will be useful for the large data transfers of the OpenLST
 }
 
+// sets the pins on the OpenLST
 void openlst_setup() {
     uart_init(OPENLST_UART_ID, OPENLST_UART_BAUD_RATE);
 
@@ -28,7 +30,8 @@ void openlst_setup() {
     uart_set_hw_flow(OPENLST_UART_ID, false, false);    // flow control
 
 }
- 
+
+// sends a string to serial, viewable through minicom 
 void uart_test() {
     sleep_ms(100);
     //uart_putc_raw(UART_ID, 'J'); // sends a J
@@ -50,22 +53,58 @@ void uart_ack_test() {
     ack_packet[5] = 0x3D;   // increments up 1 each time ??
     ack_packet[6] = 0x59;
     ack_packet[7] = 0x01;
-    ack_packet[8] = 0x10;
+    ack_packet[8] = 0x10;   // this byte can most likely be anything 
+
     uart_write_blocking(UART_ID, ack_packet, 9);
     uart_read_blocking(UART_ID, acknowledge, 9);
 }
 
+// sends a packet to get some useful telemetry info back
 void uart_get_telem() {
     sleep_ms(100);
-    uint8_t get_telem[];
-    uint8_t telem[];
+    uint8_t get_telem[9];
+    uint8_t telem[87];  // length of telem message 
 
-    get_telem[0] =
+    get_telem[0] = 0x22;
+    get_telem[1] = 0x69;
+    get_telem[2] = 0x06;
+    get_telem[3] = 0xFF;
+    get_telem[4] = 0xFF;
+    get_telem[5] = 0x3F;
+    get_telem[6] = 0x59;
+    get_telem[7] = 0x01;
+    get_telem[8] = 0x17;
 
+    uart_write_blocking(UART_ID, get_telem, 9);
+    uart_read_blocking(UART_ID, telem, 87);
 }
 
-int send_packet(uint8_t *data, int len, int dest_hwid) {
+// sends the packet through the OpenLST
+// needs data, length of data, HWID of destination
+void send_packet(uint8_t *data, int len, int dest_hwid) {
 
+    if (len > MAX_PAYLOAD_LEN) {
+
+    }
+
+    uint8_t packet[len + 5];    // length of full packet
+
+    // parts of the packet that are always the same
+    packet[0] = 0x22;
+    packet[1] = 0x69;
+    packet[2] = len;    // length of data, not full packet
+    packet[len + 3] = dest_hwid;
+    packet[len + 4] = 0x18; // random? does this matter?? TODO: test
+
+    // this loop puts the data inbetween the starting bytes and the ending bytes
+    int j = 0;
+    for(int i = 3; i < (len + 3); i++) {
+        packet[i] = data[j];
+        j++ ; 
+ 
+    }
+    // send the packet 
+    uart_write_blocking(UART_ID, packet, (len + 5));    // 3 header bytes and 2 ending bytes 
 }
 
 int main() {
@@ -73,4 +112,8 @@ int main() {
     uart_setup();
     //uart_test();
     //uart_ack_test();
+    //uart_get_telem();
+    sleep_ms(100);
+    send_packet(data,12,OPENLST_HWID);
+
 }
